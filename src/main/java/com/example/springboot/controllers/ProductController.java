ad9a9d23dd1e244201eaa793b6dbde3d2e07f52b
Gabriel Bento <gab.silva.bento@gmail.com>
2023-06-14 14:00:26 -0300

    Adicionado hateos na rota de leitura

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -22,6 +22,9 @@ import com.example.springboot.repositories.ProductRepository;
 
 import jakarta.validation.Valid;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 public class ProductController {
 	@Autowired
@@ -36,7 +39,14 @@ public class ProductController {
 	
 	@GetMapping("/products")
 	public ResponseEntity<List<ProductModel>> getAllProducts(){
-		return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+		List<ProductModel> productList = productRepository.findAll();
+		if(!productList.isEmpty()) {
+			for(ProductModel product : productList) {
+				UUID id = product.getIdProduct();
+				product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
+			}
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(productList);
 	}
 	
 	@GetMapping("/products/{id}")
@@ -45,7 +55,7 @@ public class ProductController {
 		if(productO.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
 		}
-		
+		productO.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Product List"));
 		return ResponseEntity.status(HttpStatus.OK).body(productO.get());
 	
 	}
