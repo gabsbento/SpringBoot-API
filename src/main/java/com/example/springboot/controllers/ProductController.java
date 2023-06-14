e3c166241683f5aff0b2b81ea42b0cf6ad0d0dd7
Gabriel Bento <gab.silva.bento@gmail.com>
2023-06-14 11:35:58 -0300

    Adicionado a rota de exclusao de products no banco

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,5 +61,16 @@ public class ProductController {
 		BeanUtils.copyProperties(productRecordDTO, productModel);
 		return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
 		
+	}
+	
+	
+	@DeleteMapping("products/{id}")
+	public ResponseEntity<Object> deleteProduct(@PathVariable(value="id") UUID id){
+		Optional<ProductModel> productO = productRepository.findById(id);
+		if(productO.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+		}
+		productRepository.delete(productO.get());
+		return ResponseEntity.status(HttpStatus.OK).body("Product Deleted successfully.");
 	}
 }
